b0aa4e46197d87adc17cbe555786e7fadc9ac167
Brad Parbs <brad@bradparbs.com>
2023-03-31 19:45:09 -0500

    Update

--- mh-taller-windows.user.js
+++ mh-taller-windows.user.js
@@ -6,44 +6,44 @@
 // @author       bradp
 // @namespace    bradp
 // @match        https://www.mousehuntgame.com/*
-// @icon         https://brrad.com/mouse.png
+// @icon         https://i.mouse.rip/mouse.png
 // @grant        none
 // @run-at      document-end
 // ==/UserScript==
 
 ((function () {
-	'use strict';
+  'use strict';
 
-	const style = document.createElement('style');
-	style.innerHTML = `#supplytransfer .drawer,
-	#supplytransfer .drawer .tabContent,
-	#supplytransfer .drawer .listContainer,
-	.treasureMapView-blockWrapper.tall .treasureMapView-block-content,
-	.treasureMapView-block-content.tall,
-	.treasureMapView-block-content,
-	.MHCheckoutAllRewardsPageView,
-	.treasureMapListingsView-tableView,
-	.marketplaceView-browse-content,
-	.adventureBookPopup-titleContent,
-	.treasureMapView-block-content.halfHeight,
-	.convertibleOpenView-itemContainer,
-	#messengerUINotification .notificationHeader,
-	#messengerUINotification .notificationMessageList,
-	.convertibleOpenView-itemContainer {
-		max-height: 75vh;
-		height: auto;
-	}
+  const style = document.createElement('style');
+  style.innerHTML = `#supplytransfer .drawer,
+  #supplytransfer .drawer .tabContent,
+  #supplytransfer .drawer .listContainer,
+  .treasureMapView-blockWrapper.tall .treasureMapView-block-content,
+  .treasureMapView-block-content.tall,
+  .treasureMapView-block-content,
+  .MHCheckoutAllRewardsPageView,
+  .treasureMapListingsView-tableView,
+  .marketplaceView-browse-content,
+  .adventureBookPopup-titleContent,
+  .treasureMapView-block-content.halfHeight,
+  .convertibleOpenView-itemContainer,
+  #messengerUINotification .notificationHeader,
+  #messengerUINotification .notificationMessageList,
+  .convertibleOpenView-itemContainer {
+    max-height: 75vh;
+    height: auto;
+  }
 
-	.giftSelectorView-inbox-giftContainer {
-		min-height: 300px;
-		max-height: 75vh;
-		height: auto;
-	}
+  .giftSelectorView-inbox-giftContainer {
+    min-height: 300px;
+    max-height: 75vh;
+    height: auto;
+  }
 
-	.giftSelectorView-scroller {
-		min-height: 320px;
-		max-height: 55vh;
-		height: auto;
-	}`;
-	document.head.appendChild(style);
+  .giftSelectorView-scroller {
+    min-height: 320px;
+    max-height: 55vh;
+    height: auto;
+  }`;
+  document.head.appendChild(style);
 })());
